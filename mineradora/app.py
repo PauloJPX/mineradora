5dfc460bd1a0b077864a1bea5a33dbd553dc8ef1
Paulo JPX <jpaulojpx@gmail.com>
2024-10-12 23:39:24 -0300

    seguranca e acesso

--- mineradora/app.py
+++ mineradora/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, session, redirect, url_for
+from flask import Flask, session, redirect, url_for, request
 from config import Config
 from functools import wraps
-from routes import auth, usuarios, menu, clientes
-from routes import vendas  # Adicione vendas aqui
+from routes import auth, usuarios, menu, clientes, vendas
 
 def login_required(f):
     @wraps(f)
@@ -15,11 +14,23 @@ def login_required(f):
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Registrar os blueprints
 app.register_blueprint(auth.bp)
 app.register_blueprint(usuarios.bp)
 app.register_blueprint(clientes.bp)
 app.register_blueprint(menu.bp)
-app.register_blueprint(vendas.bp)  # Registra o blueprint de vendas
+app.register_blueprint(vendas.bp)
+
+# Proteger todas as rotas do blueprint de vendas
+@app.before_request
+def require_login():
+
+    listaMenu= ['vendas.', 'usuarios.', 'menu.', 'clientes.']
+
+    for menu in listaMenu:
+        if request.endpoint and request.endpoint.startswith(menu) and 'logged_in' not in session:
+            return redirect(url_for('auth.login'))
+
 
 @app.route('/')
 @login_required
--- mineradora/routes/vendas.py
+++ mineradora/routes/vendas.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, jsonify
 from models import get_db_connection
+#from app import login_required  # Importa o decorador
 
 bp = Blueprint('vendas', __name__, url_prefix='/vendas')
 
 @bp.route('/incluir', methods=['GET', 'POST'])
+#@login_required  # Protege a rota
 def incluir_venda():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
@@ -44,6 +46,7 @@ def incluir_venda():
     return render_template('incluir_venda.html', clientes=clientes, produtos=produtos)
 
 @bp.route('/verificar_numerovenda/<int:numerovenda>', methods=['GET'])
+
 def verificar_numerovenda(numerovenda):
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
@@ -57,16 +60,18 @@ def verificar_numerovenda(numerovenda):
     return jsonify({"existe": venda_existente is not None, "venda": venda_existente})
 
 @bp.route('/excluir_venda', methods=['POST'])
+
 def excluir_venda():
     numerovenda = request.form['numerovenda_excluir']
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM vendas WHERE numerovenda = %s", (numerovenda,))
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return redirect(url_for('vendas.incluir_venda', teste='sim', msg='Venda excluida com Sucesso'))
-
+    retorno = verificar_numerovenda(numerovenda) # essa fucao retorna um jso
+    resposta = retorno.json["existe"]
+    if resposta:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM vendas WHERE numerovenda = %s", (numerovenda,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('vendas.incluir_venda', teste='sim', msg='Venda excluida com Sucesso'))
+    else:
+        return redirect(url_for('vendas.incluir_venda', teste='sim', msg='Venda nao existia'))

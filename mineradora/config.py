9757d4e4393473fdb420e312cbe29c1201c3d314
Paulo JPX <jpaulojpx@gmail.com>
2024-10-26 05:54:04 -0300

    seguranca e acesso

--- mineradora/config.py
+++ mineradora/config.py
@@ -1,8 +1,19 @@
+#    SECRET_KEY = 'sua_chave_secreta'
+#    MYSQL_HOST = 'localhost'
+#    MYSQL_USER = 'root'
+#    MYSQL_PASSWORD = '@J2ptpaulo@'
+#    MYSQL_DB = 'mineradora'
+
+
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis do .env
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'sua_chave_secreta'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '@J2ptpaulo@'
-    MYSQL_DB = 'mineradora'
-
-
+    SECRET_KEY = os.getenv('SECRET_KEY', 'sua_chave_secreta')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '@J2ptpaulo@')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'mineradora')
